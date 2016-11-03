cb95a37c5953366233b07a110d27af84a625d920
SatoShun <shun.sato1@gmail.com>
2016-11-03 07:07:22 +0000

    add holiday

--- command/holiday/holiday.go
+++ command/holiday/holiday.go
@@ -1,31 +1,62 @@
 package main
 
+import "time"
+
 var holidays []holiday
 
 type holiday interface {
-	isHoliday(y, m, d int) bool
+	isHoliday(d time.Time) bool
 }
 
 type gantan struct{}
+type kenkoku struct{}
+type shouwa struct{}
+type kenpou struct{}
+type midori struct{}
+type kodomo struct{}
+type yama struct{}
 type bunka struct{}
 type kinrou struct{}
+type tennou struct{}
 
-func (g *gantan) isHoliday(y, m, d int) bool {
-	return m == 1 && d == 1
+// TODO: umi, shunbun, shuubun, taiiku, keirou, seijin
+
+func (g *gantan) isHoliday(d time.Time) bool {
+	return d.Month() == 1 && d.Day() == 1
 }
-
-func (g *bunka) isHoliday(y, m, d int) bool {
-	return m == 11 && d == 3
+func (g *kenkoku) isHoliday(d time.Time) bool {
+	return d.Month() == 2 && d.Day() == 11
 }
-
-func (g *kinrou) isHoliday(y, m, d int) bool {
-	return m == 11 && d == 23
+func (g *shouwa) isHoliday(d time.Time) bool {
+	return d.Month() == 4 && d.Day() == 29
+}
+func (g *kenpou) isHoliday(d time.Time) bool {
+	return d.Month() == 5 && d.Day() == 3
+}
+func (g *midori) isHoliday(d time.Time) bool {
+	return d.Month() == 5 && d.Day() == 4
+}
+func (g *kodomo) isHoliday(d time.Time) bool {
+	return d.Month() == 5 && d.Day() == 5
+}
+func (g *yama) isHoliday(d time.Time) bool {
+	return d.Month() == 8 && d.Day() == 11
+}
+func (g *bunka) isHoliday(d time.Time) bool {
+	return d.Month() == 11 && d.Day() == 3
+}
+func (g *kinrou) isHoliday(d time.Time) bool {
+	return d.Month() == 11 && d.Day() == 23
+}
+func (g *tennou) isHoliday(d time.Time) bool {
+	return (d.Month() == 12 && d.Day() == 23) || (d.Month() == 4 && d.Day() == 29)
 }
 
 // IsHoliday returns holiday or not
 func IsHoliday(y, m, d int) bool {
+	dd := time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.Local)
 	for _, h := range holidays {
-		if h.isHoliday(y, m, d) {
+		if h.isHoliday(dd) {
 			return true
 		}
 	}
@@ -35,8 +66,9 @@ func IsHoliday(y, m, d int) bool {
 func init() {
 	holidays = make([]holiday, 0)
 	holidays = append(holidays,
-		&gantan{},
-		&bunka{},
-		&kinrou{},
+		&gantan{}, &kenkoku{}, &bunka{},
+		&shouwa{}, &kenpou{}, &midori{}, &kodomo{},
+		&yama{}, &kinrou{}, &tennou{},
 	)
 }
+vv
--- command/holiday/main.go
+++ command/holiday/main.go
@@ -93,12 +93,32 @@ func parse(data []byte) *calSt {
 
 func monthNormalize(m string) int {
 	switch m {
+	case "January":
+		return 1
+	case "February":
+		return 2
+	case "March":
+		return 3
 	case "April":
 		return 4
+	case "May":
+		return 5
+	case "June":
+		return 6
+	case "July":
+		return 7
+	case "August":
+		return 8
+	case "September":
+		return 9
+	case "October":
+		return 10
 	case "November":
 		return 11
+	case "December":
+		return 12
 	}
-	return 0
+	panic("unreachable")
 }
 
 // TODO: it's a hacky code
